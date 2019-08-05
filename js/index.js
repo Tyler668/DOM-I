2280978d4dfc1c79e51fae255a21040061d8b5d4
Tyler Gilchrist <tyler668@gmail.com>
2019-08-05 15:18:22 -0400

    Header, h1, button, code circle complete

--- js/index.js
+++ js/index.js
@@ -14,29 +14,61 @@ const siteContent = {
     "img-src": "img/header-img.png"
   },
   "main-content": {
-    "features-h4":"Features",
+    "features-h4": "Features",
     "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "about-h4":"About",
+    "about-h4": "About",
     "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "middle-img-src": "img/mid-page-accent.jpg",
-    "services-h4":"Services",
+    "services-h4": "Services",
     "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "product-h4":"Product",
+    "product-h4": "Product",
     "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "vision-h4":"Vision",
+    "vision-h4": "Vision",
     "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
   },
   "contact": {
-    "contact-h4" : "Contact",
-    "address" : "123 Way 456 Street Somewhere, USA",
-    "phone" : "1 (888) 888-8888",
-    "email" : "sales@greatidea.io",
+    "contact-h4": "Contact",
+    "address": "123 Way 456 Street Somewhere, USA",
+    "phone": "1 (888) 888-8888",
+    "email": "sales@greatidea.io",
   },
   "footer": {
-    "copyright" : "Copyright Great Idea! 2018"
+    "copyright": "Copyright Great Idea! 2018"
   },
 };
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+
+
+let navBar = document.querySelector("nav");
+// navBar.style.backgroundColor = "blue"
+
+
+// Setting Nav Bar Content === 
+let allA = document.querySelectorAll('nav a');
+allA[0].textContent = 'Services'; 
+allA[1].textContent = 'Product'; 
+allA[2].textContent = 'Vision'; 
+allA[3].textContent = 'Features'; 
+allA[4].textContent = 'About';
+allA[5].textContent = 'Contact'; 
+
+
+// Primary H1 ===
+let h1Text = document.querySelector('.cta-text h1');
+h1Text.setAttribute('style', 'white-space: pre;');
+h1Text.textContent = "DOM \n";
+h1Text.textContent += "IS \n"; 
+h1Text.textContent += "AWESOME"; 
+
+
+// Button ===
+let buttonUnderH1 = document.querySelector('.cta-text button');
+buttonUnderH1.textContent = "Get Started";
+
+// Circular Code Image ===
+let codeImage = document.querySelector('.cta img');
+codeImage.setAttribute('src', "img/header-img.png");
